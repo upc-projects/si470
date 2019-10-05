ff39ee5d0b25501d7a936e20dd6ae30b322590a6
Enzo Lizama <lizama.enzo@gmail.com>
2019-10-04 23:48:00 -0500

    add docs

---
+++ SoapApi/Calculador.java
@@ -0,0 +1,12 @@
+class Calculador {
+    public int suma(int a, int b){
+        return a + b;
+    }
+}
+
+// convert into web service
+// generate wsdl
+// http://localhost:8080/ws5/services/Calculador?wsdl
+
+// for rest
+// http://localhost:8080/RestDemo/rest/asistencia
